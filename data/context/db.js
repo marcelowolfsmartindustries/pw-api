01384d398152010eb088a3dc36263b2d90fa105f
Pedro Nogueira <pedromdsn.2002@hotmail.com>
2022-05-16 03:09:09 +0100

    fix: :lock: remove databse URL from hardcode

--- data/context/db.js
+++ data/context/db.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://postgres:superuser@localhost:5432/pw', {dialect: 'postgres'});
+const sequelize = new Sequelize(process.env.DB_URL, {dialect: 'postgres'});
 
 module.exports = sequelize;
